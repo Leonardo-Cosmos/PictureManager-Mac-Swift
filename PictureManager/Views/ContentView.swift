d48381ab683cbf86119a1ce893a267bf6cb61a8f
Leonardo-Cosmos <kylin_cl@hotmail.com>
2023-05-02 17:42:56 +0800

    Implement save and load window size

--- PictureManager/Views/ContentView.swift
+++ PictureManager/Views/ContentView.swift
@@ -33,3 +33,31 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//
+//class MainWindowController: NSWindowController, NSWindowDelegate {
+//    private static let windowX = "windowX"
+//    private static let windowY = "windowY"
+//    private static let windowWidth = "windowWidth"
+//    private static let windowHeight = "windowHeight"
+//    
+//    override func windowDidLoad() {
+//        super.windowDidLoad()
+//        if let window = window {
+//            let x = UserDefaults.standard.double(forKey: MainWindowController.windowX)
+//            let y = UserDefaults.standard.double(forKey: MainWindowController.windowY)
+//            let width = UserDefaults.standard.double(forKey: MainWindowController.windowWidth)
+//            let height = UserDefaults.standard.double(forKey: MainWindowController.windowHeight)
+//            window.setFrame(NSRect(x: x, y: y, width: width, height: height), display: true)
+//        }
+//    }
+//    
+//    func windowShouldClose(_ sender: NSWindow) -> Bool {
+//        let frame = sender.frame
+//        UserDefaults.standard.set(frame.origin.x, forKey: MainWindowController.windowX)
+//        UserDefaults.standard.set(frame.origin.y, forKey: MainWindowController.windowY)
+//        UserDefaults.standard.set(frame.size.width, forKey: MainWindowController.windowWidth)
+//        UserDefaults.standard.set(frame.size.height, forKey: MainWindowController.windowHeight)
+//        return true
+//    }
+//}
